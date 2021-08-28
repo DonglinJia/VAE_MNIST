ea531094e126eda1e44c2e3467c9bcded2c80b8e
Leo Jia <leojia@Leos-MacBook-Pro.local>
2021-08-28 18:32:17 -0400

    add source file

---
+++ vae.py
@@ -0,0 +1,186 @@
+from __future__ import print_function
+import torch
+import torch.utils.data
+from torch import nn, optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+from IPython.display import Image, display
+import matplotlib.pyplot as plt
+import os
+
+if not os.path.exists('results'):
+    os.mkdir('results')
+
+batch_size = 100
+latent_size = 20
+
+cuda = torch.cuda.is_available()
+device = torch.device("cuda" if cuda else "cpu")
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True,
+                   transform=transforms.ToTensor()),
+    batch_size=batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
+    batch_size=batch_size, shuffle=True, **kwargs)
+
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+        self.reLU = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+        self.fc_encode = nn.Linear(784, 400)
+        self.fc_mu = nn.Linear(400, latent_size)
+        self.fc_var = nn.Linear(400, latent_size)
+
+        self.fc_decode1 = nn.Linear(latent_size, 400)
+        self.fc_decode2 = nn.Linear(400, 784)
+
+
+    def encode(self, x):
+        #The encoder will take an input of size 784, and will produce two vectors of size latent_size (corresponding to the coordinatewise means and log_variances)
+        #It should have a single hidden linear layer with 400 nodes using ReLU activations, and have two linear output layers (no activations)
+        # print("Input shape: " + str(x.shape))
+        h1 = self.reLU(self.fc_encode(x))
+        return self.fc_mu(h1), self.fc_var(h1)
+
+    def reparameterize(self, means, log_variances):
+        #The reparameterization module lies between the encoder and the decoder
+        #It takes in the coordinatewise means and log-variances from the encoder (each of dimension latent_size), and returns a sample from a Gaussian with the corresponding parameters
+        # vector_size = log_variances.size()
+        # eps = Variable(torch.FloatTensor(vector_size).normal_())
+        # std = means.mul(0.5).exp_()
+        std = torch.exp(0.5*log_variances)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(means)
+
+    def decode(self, z):
+        #The decoder will take an input of size latent_size, and will produce an output of size 784
+        #It should have a single hidden linear layer with 400 nodes using ReLU activations, and use Sigmoid activation for its outputs
+        h1 = self.reLU(self.fc_decode1(z))
+        return self.sigmoid(self.fc_decode2(h1))
+
+    def forward(self, x):
+        #Apply the VAE encoder, reparameterization, and decoder to an input of size 784
+        #Returns an output image of size 784, as well as the means and log_variances, each of size latent_size (they will be needed when computing the loss)
+        mu, log_var = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, log_var)
+        # print("Reconstruction shape: " + str(reconsructed_image.shape))
+        return self.decode(z), mu, log_var
+
+def vae_loss_function(reconstructed_x, x, means, log_variances):
+    #Compute the VAE loss
+    #The loss is a sum of two terms: reconstruction error and KL divergence
+    #Use cross entropy loss between x and reconstructed_x for the reconstruction error (as opposed to L2 loss as discussed in lecture -- this is sometimes done for data in [0,1] for easier optimization)
+    #The KL divergence is -1/2 * sum(1 + log_variances - means^2 - exp(log_variances)) as described in lecture
+    #Returns loss (reconstruction + KL divergence) and reconstruction loss only (both scalars)
+
+    # reconstruction loss
+    BCE = F.binary_cross_entropy(reconstructed_x, x.view(-1, 784), reduction='sum')
+
+    # KL loss
+    KL_loss = -0.5 * torch.sum(1 + log_variances - means.pow(2) - log_variances.exp())
+    return BCE + KL_loss, BCE
+
+
+def train(model, optimizer, epoch):
+    #Trains the VAE for one epoch on the training dataset
+    #Returns the average (over the dataset) loss (reconstruction + KL divergence) and reconstruction loss only (both scalars)
+    model.train()
+    avg_train_loss = 0
+    avg_train_reconstruction_loss = 0
+    for batch_index, (data, _) in enumerate(train_loader):
+            data = data.to(device)
+            optimizer.zero_grad()
+            recon_batch, mu, log_var = model(data)
+
+            loss, tmp_reconstruction_loss = vae_loss_function(recon_batch, data, mu, log_var)
+            loss.backward()
+            # tmp_reconstruction_loss.backward()
+            optimizer.step()
+
+            avg_train_loss += loss.item()
+            avg_train_reconstruction_loss += tmp_reconstruction_loss.item()
+
+            if batch_index % 100 == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\t Total Loss: {:.6f} \t Reconstruction Loss: {:.6f}'.format(
+                    epoch, batch_index * len(data), len(train_loader.dataset),
+                    100. * batch_index / len(train_loader), loss.item() / len(data), 
+                    tmp_reconstruction_loss.item() / len(data)))
+
+    # avg_train_loss /= len(train_loader)
+    # avg_train_reconstruction_loss /= len(train_loader)
+    return avg_train_loss / len(train_loader.dataset), avg_train_reconstruction_loss / len(train_loader.dataset)
+
+def test(model, epoch):
+    #Runs the VAE on the test dataset
+    #Returns the average (over the dataset) loss (reconstruction + KL divergence) and reconstruction loss only (both scalars)
+    model.eval()
+    avg_test_loss = 0
+    avg_test_reconstruction_loss = 0
+    with torch.no_grad():
+        for batch_idx, (data, _) in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar = model(data)
+
+            tmp_loss, tmp_reconstruction_loss = vae_loss_function(recon_batch, data, mu, logvar)
+
+            avg_test_loss += tmp_loss.item()
+            avg_test_reconstruction_loss += tmp_reconstruction_loss.item()
+
+    avg_test_loss /= len(test_loader.dataset)
+    avg_test_reconstruction_loss /= len(test_loader.dataset)
+
+    print('\nEpoch {}: Test set: Average loss: {:.6f}, Average reconstruction loss: {:.6f} \n'.format(epoch, 
+        avg_test_loss, avg_test_reconstruction_loss))
+    
+    return avg_test_loss, avg_test_reconstruction_loss
+
+epochs = 50
+avg_train_losses = []
+avg_train_reconstruction_losses = []
+avg_test_losses = []
+avg_test_reconstruction_losses = []
+
+vae_model = VAE().to(device)
+vae_optimizer = optim.Adam(vae_model.parameters(), lr=1e-3)
+
+for epoch in range(1, epochs + 1):
+    avg_train_loss, avg_train_reconstruction_loss = train(vae_model, vae_optimizer, epoch)
+    avg_test_loss, avg_test_reconstruction_loss = test(vae_model, epoch)
+    
+    avg_train_losses.append(avg_train_loss)
+    avg_train_reconstruction_losses.append(avg_train_reconstruction_loss)
+    avg_test_losses.append(avg_test_loss)
+    avg_test_reconstruction_losses.append(avg_test_reconstruction_loss)
+
+    with torch.no_grad():
+        sample = torch.randn(64, latent_size).to(device)
+        sample = vae_model.decode(sample).cpu()
+        save_image(sample.view(64, 1, 28, 28),
+                   'results/sample_' + str(epoch) + '.png')
+        print('Epoch #' + str(epoch))
+        display(Image('results/sample_' + str(epoch) + '.png'))
+        print('\n')
+
+fig_train_loss = plt.figure()
+plt.plot(avg_train_reconstruction_losses)
+plt.title('Training Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch #')
+plt.show()
+fig_train_loss.savefig('Train_loss.png')
+
+fig_test_loss = plt.figure()
+plt.plot(avg_test_reconstruction_losses)
+plt.title('Test Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch #')
+plt.show()
+fig_test_loss.savefig('Test_loss.png')
